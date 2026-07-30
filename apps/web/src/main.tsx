920d9fd98e819d152c66443e6e161241d6378b36
Lugbinlaye <pauldave997@gmail.com>
2026-07-30 17:25:07 +0100

    Load workforce lifecycle styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/base.css";
 import "./styles/shell.css";
 import "./styles/views.css";
+import "./styles/workforce-lifecycle.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><App /></React.StrictMode>,
