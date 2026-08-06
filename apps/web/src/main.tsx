2cb0fea58cec83ef530b0af1eab0650ddfccd7d2
Lugbinlaye <pauldave997@gmail.com>
2026-08-06 10:42:01 +0100

    Load workforce and customer intelligence styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/base.css";
 import "./styles/shell.css";
 import "./styles/views.css";
 import "./styles/workforce-lifecycle.css";
+import "./features/customer-intelligence/customer-intelligence.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><App /></React.StrictMode>,
